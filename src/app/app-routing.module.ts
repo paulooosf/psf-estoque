36d07b0fec7c661d36b0d3dd49c3e8b93e49ec0e
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-06-12 14:39:29 -0300

    configurando e aplicando roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ListarCategoriasComponent } from './pages/listar-categorias/listar-categorias.component';
+import { CriarCategoriaComponent } from './pages/criar-categoria/criar-categoria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+      redirectTo: 'dashboard',
+      pathMatch: 'full'
+  },
+  {
+      path: 'dashboard',
+      component: DashboardComponent
+  },
+  {
+      path: 'categorias',
+      component: ListarCategoriasComponent
+  },
+  {
+      path: 'criar-categoria',
+      component: CriarCategoriaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ListarCategoriasComponent } from './pages/listar-categorias/listar-categorias.component';
+import { CriarCategoriaComponent } from './pages/criar-categoria/criar-categoria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ListarCategoriasComponent,
+    CriarCategoriaComponent
   ],
   imports: [
     BrowserModule,
