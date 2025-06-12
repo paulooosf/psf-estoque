7a7d15e6642e5c2e41c51febc42188923de563cf
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-06-12 14:39:04 -0300

    criando componente de criar categorias

---
+++ src/app/pages/criar-categoria/criar-categoria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-categoria',
+  templateUrl: './criar-categoria.component.html',
+  styleUrls: ['./criar-categoria.component.css']
+})
+export class CriarCategoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
