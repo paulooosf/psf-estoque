4b77b6781d6803f13605ddeb7655469b1a21788a
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-06-13 13:25:32 -0300

    estruturando formulario de criiação de categoria

--- src/app/pages/criar-categoria/criar-categoria.component.ts
+++ src/app/pages/criar-categoria/criar-categoria.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CategoriaService } from 'src/app/shared/services/categoria.service';
 
 @Component({
   selector: 'app-criar-categoria',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarCategoriaComponent implements OnInit {
 
-  constructor() { }
+  formulario!: FormGroup;
+
+  constructor(private service: CategoriaService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, 
+        Validators.minLength(3),
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])]
+    })
+  }
+
+  incluirCategoria() {
+    if (this.formulario.valid) {
+      this.service.incluir(this.formulario.value).subscribe(() => this.router.navigate(['/listar-categorias']))
+    } else {
+      alert('Preencha o formulário corretamente!')
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listar-categorias'])
+  }
+
+  habilitarBotao() {
+    if (this.formulario.valid) {
+      return 'rounded-md py-2 px-4 bg-lime-200 hover:bg-lime-500 text-black hover:text-white'
+    } else {
+      return 'rounded-md py-2 px-4 hover:bg-transparent border border-stone-600 text-stone-300'
+    }
   }
 
 }
