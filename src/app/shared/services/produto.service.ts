c520843c890302266ce0758ee64f3fd79606e2f2
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-06-20 15:07:43 -0300

    criando service de produto

---
+++ src/app/shared/services/produto.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaginacaoDto } from '../models/common/paginacao.dto';
+import { ProdutoSaidaDto } from '../models/produto/produto.saida.dto';
+import { ProdutoEntradaDto } from '../models/produto/produto.entrada.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  private readonly API = 'http://localhost:5051/api/produto'
+
+  constructor(private http: HttpClient) { }
+
+  listar(pagina: number): Observable<PaginacaoDto<ProdutoSaidaDto>> {
+
+    let params = new HttpParams()
+                        .set('pagina', pagina)
+                        .set('tamanhoPagina', 10)
+
+    return this.http.get<PaginacaoDto<ProdutoSaidaDto>>(this.API, { params })
+  }
+
+  buscar(id: number): Observable<ProdutoSaidaDto> {
+    return this.http.get<ProdutoSaidaDto>(`${this.API}/${id}`)
+  }
+
+  buscarPorCategoria(idCategoria: number): Observable<ProdutoSaidaDto> {
+    return this.http.get<ProdutoSaidaDto>(`${this.API}/porCategoria/${idCategoria}`)
+  }
+
+  incluir(produto: ProdutoEntradaDto): Observable<ProdutoSaidaDto> {
+      return this.http.post<ProdutoSaidaDto>(this.API, produto)
+  }
+
+  adicionarQuantidade(id: number, quantidade: number): Observable<ProdutoSaidaDto> {
+    return this.http.put<ProdutoSaidaDto>(`${this.API}/adicionarQuantidade/${id}`, quantidade)
+  }
+
+  removerQuantidade(id: number, quantidade: number): Observable<ProdutoSaidaDto> {
+    return this.http.put<ProdutoSaidaDto>(`${this.API}/removerQuantidade/${id}`, quantidade)
+  }
+  
+  alterar(produto: ProdutoEntradaDto, id: number): Observable<ProdutoSaidaDto> {
+    return this.http.put<ProdutoSaidaDto>(`${this.API}/${id}`, produto)
+  }
+
+  excluir(id: number): Observable<ProdutoSaidaDto> {
+    return this.http.delete<ProdutoSaidaDto>(`${this.API}/${id}`)
+  }
+}
