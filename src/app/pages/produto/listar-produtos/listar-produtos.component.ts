85a9d56eca9a23016aa48f0d9ec87f7ccb0ba988
Paulo Henrique <phferreira.dsn.pack@alterdata.com.br>
2025-06-20 15:08:00 -0300

    criando pagina de listagem de produtos

--- src/app/pages/produto/listar-produtos/listar-produtos.component.ts
+++ src/app/pages/produto/listar-produtos/listar-produtos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginacaoDto } from 'src/app/shared/models/common/paginacao.dto';
+import { ProdutoSaidaDto } from 'src/app/shared/models/produto/produto.saida.dto';
+import { ProdutoService } from 'src/app/shared/services/produto.service';
 
 @Component({
   selector: 'app-listar-produtos',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarProdutosComponent implements OnInit {
 
-  constructor() { }
+  listaProdutos: PaginacaoDto<ProdutoSaidaDto> = {
+    itens: [],
+    numeroPagina: 1,
+    tamanhoPagina: 10,
+    totalPaginas: 0,
+    totalItens: 0,
+    temPaginaAnterior: false,
+    temProximaPagina: false
+  }
+  paginaAtual: number = 1
+  mostrarModalQuantidade: boolean = false
+  produtoSelecionado?: ProdutoSaidaDto
+
+  constructor(private service: ProdutoService) { }
 
   ngOnInit(): void {
+    this.service.listar(this.paginaAtual).subscribe((listaProdutos) => {
+      this.listaProdutos = listaProdutos
+    })
+  }
+
+  carregarMaisProdutos() {
+    this.service.listar(++this.paginaAtual).subscribe((listaProdutos) => {
+      this.listaProdutos.itens.push(...listaProdutos.itens)
+      this.listaProdutos.temProximaPagina = listaProdutos.temProximaPagina
+    })
+  }
+
+  abrirModalQuantidade(produto: ProdutoSaidaDto): void {
+    this.produtoSelecionado = produto
+    this.mostrarModalQuantidade = true
+  }
+
+  fecharModalQuantidade(): void {
+    this.mostrarModalQuantidade = false
+    this.produtoSelecionado = undefined
   }
 
 }
